1652323c2c87d234ff683dc9cf2f8d14a223d088
David S. Jackson <deepbsd@yahoo.com>
2023-01-30 20:39:23 -0500

    just change the vowel counter program in lesson 4 to only show the final count, not the incrementing of the variable.

--- ostPython1/lesson4/vowel_counter.py
+++ ostPython1/lesson4/vowel_counter.py
@@ -9,4 +9,8 @@ vcount = 0
 for c in s:
     if c in "aeiouAIEOU": 
         vcount += 1
-    print("C is ",c, "; Vowel count:",vcount )
+    #print("C is ",c, "; Vowel count:",vcount )
+
+# Assume we no longer want to see the incrementing of the vowel count,
+# just show us the final count of the vowels
+print("You have ",vcount, " vowels!")
